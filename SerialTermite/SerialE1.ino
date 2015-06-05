dc728875190aeafac8628378546ee5b1ff41d322
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-06-05 11:50:20 -0500

    Update SerialE1.ino

--- SerialTermite/SerialE1.ino
+++ SerialTermite/SerialE1.ino
@@ -1,4 +1,4 @@
-unsigned int var1 = 43223;  //2 bytes
+unsigned int var1 = 1231;  //2 bytes
 double var2 = 1231241.12111;  //cuantos bytes? 4 bytes - correcto!!!
 int var3 = -12321;  //2 bytes
 char var4 = 'x';  //1 byte
@@ -7,17 +7,17 @@ unsigned char temporal = 0;
 
 void setup(void){
   Serial.begin(9600);
-  Serial.println("HeTPro");
+ // Serial.println("HeTPro");
 }
 
 void loop(void){
-  Serial.println("Test serial"); //cuantas veces se imprimira este texto???    R. 1 sola vez
+ // Serial.println("Test serial"); //cuantas veces se imprimira este texto???    R. 1 sola vez
    //Porque?: Despues de esta instrucción viene un ciclo for infinito....
   for(;;){
     if(Serial.available() == 1){
      dato_recibido = Serial.read(); 
       switch(dato_recibido){
-       case 'A':   Serial.write(var1); Serial.write((unsigned char)var1>>8);    break;
+       case 'A':  Serial.write(var1);   Serial.write((unsigned int)var1>>8);    break;
        case 'B':  
                     temporal = var2;
                     Serial.write(temporal);
@@ -26,7 +26,7 @@ void loop(void){
                     temporal = (unsigned int) var2 >> 16;
                     Serial.write(temporal); 
                break;
-       case 'C':   Serial.write(var3); Serial.write((unsigned char) var3>>8);     break;
+       case 'C':   Serial.write(var3); Serial.write((unsigned int) var3>>8);     break;
        case 'D':   Serial.write(var4);    break;
        case 'E':  Serial.println("Gracias por usar nuestor administrador serial"); break;
        default:  Serial.println("Error 5202"); 
@@ -44,4 +44,4 @@ void loop(void){
 
 //pREGUNTA:
 // Cuantos bytes se requieren para transmitir la siguiente información: Serial.println("125625.232234");   R: 13 BYTES, 1 por cada aracter
-//Pero, lo mas eficiente es enviar la variable, no su conversion a caracter, ya que la variable, por ejemplo var2, ocupa 4 bytes y representa praticamente la misma informacion. Pero esa informacion no se puede interpretar directamente. 
+//Pero, lo mas eficiente es enviar la variable, no su conversion a caracter, ya que la variable, por ejemplo var2, ocupa 4 bytes y representa praticamente la misma informacion. Pero esa informacion no se puede interpretar directamente.
