91a099e8f8a85389dd452ebe5fc7d9cc2196a678
esmarr58 <ruben.estrada@hetpro.com.mx>
2015-06-16 21:04:28 -0500

    Create ArduinoLoggerDHT11.ino

---
+++ SerialTermite/ArduinoLoggerDHT11.ino
@@ -0,0 +1,69 @@
+#include <rDHT11.h>
+
+// Establecemos el pin digital al que conectamos la línea de datos 
+// del sensor
+int rDHT11pin = 12;
+// Declaramos una variable que será la utilizada para 
+rDHT11 DHT11(rDHT11pin);
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int result = DHT11.update();
+  int temperatura = 0;
+  int humedad = 0;
+  char dato = 0;
+    switch (result)
+  {
+    case rDHT11Definitions::OK: 
+		//Serial.println("OK"); 
+                // Mostramos los valores recogidos
+                //Serial.print("Humidity (%): ");
+               humedad = 10*((float)DHT11.getHumidity());
+              //  Serial.print("Temperature (oC): ");
+               temperatura = 10*((float)DHT11.getCelsius());
+            //    Serial.print("Dew Point (oC): ");
+              //  Serial.println(DHT11.getDewPoint());
+              //Serial.println(humedad);
+              //Serial.println(temperatura);
+		break;
+    case rDHT11Definitions::CHECKSUM_ERROR: 
+		humedad = 1004;
+                temperatura = 1004;
+		break;
+    case rDHT11Definitions::TIMEOUT_ERROR: 
+		humedad = 1002;
+                temperatura = 1002;
+		break;
+    default: 
+		humedad = 1003;
+                temperatura = 1003;
+		break;
+  }
+  
+  // Necesitamos esperar dos segundos entre dos peticiones de datos
+  // consecutivas
+  delay(2000);
+  
+  if(Serial.available() == 1){
+   dato = Serial.read();
+   if(dato == 'H'){
+       // Serial.println(humedad);
+         Serial.write(humedad); Serial.write((unsigned int) humedad>>8); 
+     }
+   else if(dato == 'T'){
+        //Serial.println(temperatura);
+         Serial.write(temperatura); Serial.write((unsigned int) temperatura>>8); 
+     }  
+   else {
+      //Serial.println("error");
+          Serial.write(0); Serial.write((unsigned int) 0>>8); 
+       }  
+  }
+ 
+ 
+}
